71ba9b15d527cd164751b7381aa8021cced6d80d
ERICK1766 <guzmanquispeerickivan@gmail.com>
2025-08-27 23:30:18 -0400

    feat: devuelve '1' para input 1  test

---
+++ src/FizzBuzz.test.js
@@ -0,0 +1,5 @@
+const fizzbuzz = require("../src/fizzbuzz");
+
+test("debería devolver '1' cuando el número es 1", () => {
+expect(fizzbuzz(1)).toBe("1");
+});
---
+++ src/fizzbuzz.js
@@ -0,0 +1,6 @@
+module.exports = fizzbuzz;
+
+function fizzbuzz(n) {
+if (n === 1) return "1";
+}
+
