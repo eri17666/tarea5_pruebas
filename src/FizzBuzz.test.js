da565d723c72b5a71c46d2336e6e38f0f7ac1542
ERICK1766 <guzmanquispeerickivan@gmail.com>
2025-08-28 00:01:37 -0400

    feat: retorna 'Buzz' para multiplos de 5

--- src/FizzBuzz.test.js
+++ src/FizzBuzz.test.js
@@ -11,3 +11,7 @@ expect(fizzbuzz(2)).toBe("2");
 test("debería devolver 'Fizz' cuando el numero es multiplo de 3", () => {
 expect(fizzbuzz(3)).toBe("Fizz");
 });
+
+test("debería devolver 'Buzz' cuando el numero es múltiplo de 5", () => {
+expect(fizzbuzz(5)).toBe("Buzz");
+});
